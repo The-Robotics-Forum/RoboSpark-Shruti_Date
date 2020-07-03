506f1b19fe4cdbae8b18ee3e06a74caf1864f39e
Rutika <rutikad2001@gmail.com>
2020-07-03 12:55:55 +0530

    2-7-2020_DS_Task-1_Rutika_Deshmukh

---
+++ Struct_student_Task.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+    struct Student 
+    {
+        int ID;
+        char Name[20];
+        int Year;
+        float CGPA;
+    };
+
+
+int main()
+{
+    Struct Student ;
+
+}
